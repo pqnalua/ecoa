2078b7833ce933ba2dd3ce4fbd252b3e8c63d9f8
pqnalua <pquenalua@outlook.com>
2026-08-31 16:53:01 -0300

    atualização

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,20 +1,7 @@
-import { IonicModule } from '@ionic/angular/lazy';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab2PageRoutingModule
-  ],
-  declarations: [Tab2Page]
+  imports: [Tab2PageRoutingModule]
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonAvatar,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonAvatar,
+    IonList,
+    IonItem,
+    IonIcon,
+    IonLabel,
+  ],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  goTo(route: string) {
+    this.router.navigate([`/${route}`]);
+  }
+
+  logout() {
+    console.log('Usuário deslogado');
+    this.router.navigate(['/login']);
+  }
 
 }
